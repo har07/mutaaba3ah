1ccb3e47f8f697d7b3c38645c4ce51ae4947c724
har07 <dev.har07@gmail.com>
2016-01-24 10:42:44 +0700

    home with working CSS+JS

---
+++ scaffold/views.py
@@ -0,0 +1,15 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from google.appengine.api import users
+
+def login(request):
+    """Redirects to the Google App Engine authentication page."""
+
+    url = users.create_login_url(dest_url=request.GET.get('next'))
+    return HttpResponseRedirect(url)
+
+def logout(requesr):
+    """Redirects to the homepage after logging the user out."""
+
+    url = users.create_logout_url(reverse('home'))
+    return HttpResponseRedirect(url)
