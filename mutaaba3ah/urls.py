a84ed3a50d0777c87d6d8b3c28f0aa5171346076
har07 <dev.har07@gmail.com>
2016-01-24 13:16:07 +0700

    add navigation bar di halaman mutaaba'ah a.k.a coming soon

--- mutaaba3ah/urls.py
+++ mutaaba3ah/urls.py
@@ -3,4 +3,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('mutaaba3ah.views',
     url(r'^$', TemplateView.as_view(template_name="mutaaba3ah/index.html"), name='mutaaba3ah'),
+    url(r'^soon/$', TemplateView.as_view(template_name="mutaaba3ah/soon.html"), name='soon'),
 )
