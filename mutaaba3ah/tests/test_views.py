4a39a221bc92d465777c7171e58dada5332c25f6
har07 <dev.har07@gmail.com>
2016-02-07 12:41:17 +0700

    initial commit

--- mutaaba3ah/tests/test_views.py
+++ mutaaba3ah/tests/test_views.py
@@ -21,9 +21,14 @@ class Mutaaba3ahViewsTest(TestCase):
     def test_display_index(self):
         response = self.client.get(reverse('mutaaba3ah'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'mutaaba3ah/index.html')
+        self.assertTemplateUsed(response, 'mutaaba3ah/create_or_edit_entry.html')
 
-    def test_display_under_construction(self):
-        response = self.client.get(reverse('mutaaba3ah/soon'))
+    def test_display_report(self):
+        response = self.client.get(reverse('mutaaba3ah/report'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'mutaaba3ah/soon.html')
+        self.assertTemplateUsed(response, 'mutaaba3ah/report.html')
+
+    def test_get_report_content(self):
+        response = self.client.get(reverse('mutaaba3ah/get_report_content'))
+        self.assertEqual(response.status_code, 200)
+        self.assertInHTML("report_content", response.body)
